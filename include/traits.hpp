89c662d0792fa111dd33f97855873ae781679068
jlutrera <informaticajlu@hgmail.com>
2026-01-30 16:53:58 +0100

    cambios en comentarios

--- include/traits.hpp
+++ include/traits.hpp
@@ -4,21 +4,25 @@
 #include <type_traits>
 
 template<typename T>
-struct is_complex {
+struct is_complex
+{
     static const bool value = false;
 };
 
 template<>
-struct is_complex<std::complex<float>> {
+struct is_complex<std::complex<float>>
+{
     static const bool value = true;
 };
 
 template<>
-struct is_complex<std::complex<double>> {
+struct is_complex<std::complex<double>>
+{
     static const bool value = true;
 };
 
 template<typename T>
-struct is_complex_v {
+struct is_complex_v
+{
     static const bool value = is_complex<T>::value;
 };
